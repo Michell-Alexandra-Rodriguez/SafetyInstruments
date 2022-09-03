2031aab985a3348224afc5287fcd2aa6218520a7
Michell-Alexandra-Rodriguez <michi200913@gmail.com>
2022-09-03 17:23:03 -0500

    Las validaciones de los campos fueron mejoradas de forma satisfactoria

--- user/models.py
+++ user/models.py
@@ -1,32 +1,52 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
+from generalvalidations.database_validations import characters_size_should_be_grater_than_two, \
+    should_be_alphabetic, should_not_have_spaces, is_number, cellphone_number_size_is_ten, \
+    identification_number_size_should_be_greater_than_seven, should_not_have_special_characters
 
 
 class IdentificationType(models.Model):
-    type = models.CharField(max_length=45)
+    type = models.CharField(max_length=4, unique=True, validators=[
+        characters_size_should_be_grater_than_two, should_be_alphabetic, should_not_have_spaces
+    ])
 
     def __str__(self):
         return self.type
 
 
-class Profile(models.Model):
+class UserAdditionalInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.CharField(max_length=100, unique=True)
     address = models.CharField(max_length=100)
-    cellphone = models.CharField(max_length=15)
+    cellphone = models.CharField(max_length=10, validators=[is_number, cellphone_number_size_is_ten])
     picture = models.ImageField(upload_to='user/picture', blank=True, null=True)
     identification_type = models.ForeignKey(IdentificationType, on_delete=models.PROTECT)
-    identification_number = models.IntegerField()
+    identification_number = models.CharField(max_length=10,
+                                             validators=[
+                                                 is_number,
+                                                 identification_number_size_should_be_greater_than_seven
+                                             ])
 
     def __str__(self):
         return self.user.username
 
-    class Meta:
-        verbose_name_plural = "User Information"
-
 
 class Client(models.Model):
-    people = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50, validators=[
+        should_be_alphabetic, characters_size_should_be_grater_than_two, should_not_have_special_characters
+    ])
+    last_name = models.CharField(max_length=50, validators=[
+        should_be_alphabetic, characters_size_should_be_grater_than_two, should_not_have_special_characters
+    ])
+    email = models.EmailField(max_length=90, unique=True)
+    address = models.CharField(max_length=100)
+    cellphone = models.CharField(max_length=10, validators=[is_number, cellphone_number_size_is_ten])
+    identification_type = models.ForeignKey(IdentificationType, on_delete=models.PROTECT)
+    identification_number = models.CharField(max_length=10,
+                                             validators=[
+                                                 is_number,
+                                                 identification_number_size_should_be_greater_than_seven
+                                             ])
 
     def __str__(self):
-        return self.people.email
+        return self.name
